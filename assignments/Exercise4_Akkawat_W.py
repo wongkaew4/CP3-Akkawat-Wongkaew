8671b7f2052cb6307ad3a8fd590d4757c078380b
Akkawat Wongkaew <wongkaew.f@gmail.com>
2021-02-11 04:53:28 +0700

    Create Exercise 4
    print score

---
+++ assignments/Exercise4_Akkawat_W.py
@@ -0,0 +1,10 @@
+feScore = 60
+gbScore = 82
+icsScore = 87
+cpScore = 77
+
+print("---Your Score---")
+print("Foundation English :", feScore)
+print("General Business :", gbScore)
+print("Introduction to Computer Systems :", icsScore)
+print("Computer Programming :", cpScore)
