ba4cedf5c64e4b6260223a94b857401c1c5b0e83
Akkawat Wongkaew <wongkaew.f@gmail.com>
2021-02-12 17:24:06 +0700

    Create Exercise 5

---
+++ assignments/Exercise5_1_Akkawat_W.py
@@ -0,0 +1,12 @@
+x = int(input("input 1st number?"))
+y = int(input("input 2nd number?"))
+
+plus = x + y
+minus = x - y
+multiply = x * y
+divide = x / y
+
+print(x, "+", y, "=", plus)
+print(x, "-", y, "=", minus)
+print(x, "*", y, "=", multiply)
+print(x, "/", y, "=", divide)
