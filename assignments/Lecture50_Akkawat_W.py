8427ade6b95657fa8c231be5dfe74e2e0ac63c8e
Akkawat Wongkaew <wongkaew.f@gmail.com>
2021-02-14 14:15:56 +0700

    Create Lecture 50
    def Arithmetic Operator
    (plus minus multiply divide)

---
+++ assignments/Lecture50_Akkawat_W.py
@@ -0,0 +1,13 @@
+def plusNumber(x,y):
+    print(x+y)
+def minusNumber(x,y):
+    print(x-y)
+def multiplyNumber(x,y):
+    print(x*y)
+def divideNumber(x,y):
+    print(x/y)
+
+plusNumber(10,5)
+minusNumber(10,5)
+multiplyNumber(10,5)
+divideNumber(10,5)
